7f85fb6de7ed3e02cf0004a64be9e213d45a8ce7
Muhammad Osman <139002050+MuhammadOsman381@users.noreply.github.com>
2024-08-21 08:12:02 +0500

    some bugs are fixed

--- src/app/main/liked-videos/page.jsx
+++ src/app/main/liked-videos/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Footer from "@/components/Footer";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
@@ -7,61 +7,63 @@ import { FaEye } from "react-icons/fa6";
 import { FaHeart } from "react-icons/fa";
 import { PiTelevisionFill } from "react-icons/pi";
 
+const page = () => {
+  const router = useRouter();
+  const [likedPosts, setLikedPosts] = useState([]);
 
-const page =  () => {
-    const router = useRouter();
-    const [likedPosts,setLikedPosts] = useState([]);
+  const getLikedPosts = async () => {
+    const response = await axios("/api/user/liked-posts");
+    console.log(response.data.likedPosts);
+    setLikedPosts(response.data.likedPosts);
+  };
 
-    const getLikedPosts = async () =>{
-        const response = await axios("/api/user/liked-posts");
-        console.log(response.data.likedPosts);
-        setLikedPosts(response.data.likedPosts);
-    }
+  useEffect(() => {
+    getLikedPosts();
+  }, []);
 
-    useEffect( ()=>{
-        getLikedPosts()
-    },[])
-
-
-    const truncate = (words, length) => {
-        const wordArray = words.split(" ");
-        let truncated = "";
-        if (wordArray.length == length) {
-          truncated = words;
-          return truncated;
-        } else {
-          wordArray.map((items) => {
-            if (truncated.split(" ").length + 1 <= length) {
-              truncated = truncated + (truncated ? " " : "") + items;
-            }
-          });
-          return truncated + "...";
+  const truncate = (words, length) => {
+    const wordArray = words.split(" ");
+    let truncated = "";
+    if (wordArray.length == length) {
+      truncated = words;
+      return truncated;
+    } else {
+      wordArray.map((items) => {
+        if (truncated.split(" ").length + 1 <= length) {
+          truncated = truncated + (truncated ? " " : "") + items;
         }
-      };
+      });
+      return truncated + "...";
+    }
+  };
 
-    const single_post = (value) => {
-        console.log(value);
-        localStorage.setItem("postID", value);
-        router.push("/main/single-post");
-      };
+  const single_post = (value) => {
+    console.log(value);
+    localStorage.setItem("postID", value);
+    router.push("/main/single-post");
+  };
 
   return (
     <div>
       <div className="min-h-[100vh] p-10">
         <div className="flex flex-row flex-wrap items-center justify-center gap-10">
-          {likedPosts?.length > 0 &&
+          {likedPosts?.length > 0 ? (
             likedPosts?.map((items) => (
               <div className="hover:scale-105 transition-transform card bg-base-100 image-full w-96 shadow-xl">
                 <figure>
                   <img src={items.thumbnail} alt="Shoes" />
                 </figure>
                 <div className="card-body ">
-                  <h2 className="card-title hover:scale-105 transition-transform">{truncate(items.title, 5)}</h2>
-                  <p className="hover:scale-105 transition-transform" >{truncate(items.description, 4)}</p>
+                  <h2 className="card-title hover:scale-105 transition-transform">
+                    {truncate(items.title, 5)}
+                  </h2>
+                  <p className="hover:scale-105 transition-transform">
+                    {truncate(items.description, 4)}
+                  </p>
 
                   <div className="flex flex-row gap-5">
                     <span className="hover:scale-105 transition-transform flex flex-row items-center justify-center gap-1 w-7 ">
-                      <p >
+                      <p>
                         <FaHeart className="text-red-600" />
                       </p>
 
@@ -79,14 +81,18 @@ const page =  () => {
 
                   <div className=" card-actions justify-end">
                     <button
-                    onClick={()=>single_post(items?._id)}
-                     className=" hover:scale-110 transition-transform btn btn-error btn-sm text-white ">
+                      onClick={() => single_post(items?._id)}
+                      className=" hover:scale-110 transition-transform btn btn-error btn-sm text-white "
+                    >
                       Watch <FaEye />
                     </button>
                   </div>
                 </div>
               </div>
-            ))}
+            ))
+          ) : (
+            <div>No liked posts are available on your account!</div>
+          )}
         </div>
       </div>
       <Footer />
